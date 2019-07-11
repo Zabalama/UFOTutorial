08761b940b036a370374c791f84c859c714d0e9e
Zabalama <47012222+Zabalama@users.noreply.github.com>
2019-07-11 19:41:39 -0400

    First commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float Speed;
+    public Text countText;
+    public Text winText;
+    private Rigidbody2D rb2d;
+    private int count;
+    void Start ()
+    {
+        rb2d =  GetComponent<Rigidbody2D> ();
+        count = 0;
+        winText.text = "";
+
+        SetCountText ();
+
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb2d.AddForce (movement * Speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Pick up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+        count = count + 1;
+        SetCountText();
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString ();
+        
+        if(count >= 12)
+        winText.text = "You Win!";
+    }
+    void Update() 
+    {
+        if (Input.GetKey("escape"))
+     Application.Quit();
+    }
+}
+
+
